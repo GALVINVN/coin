cc84c3b658ffca15192ee9911f56e6ed86115865
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-07 16:26:31 +0700

    XRPSETUP.ps1

--- XRPSETUP.ps1
+++ XRPSETUP.ps1
@@ -4,7 +4,7 @@ Invoke-WebRequest -Uri 'https://www.win-rar.com/fileadmin/winrar-versions/winrar
 Start-Process -FilePath 'C:\winrar-x64-701.exe' -ArgumentList '/S' -Verb RunAs
 Invoke-WebRequest -Uri https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-gcc-win64.zip -OutFile C:\xmrig-6.22.2-gcc-win64.zip
 & 'C:\Program Files\WinRAR\WinRAR.exe' x -ibck -y 'C:\xmrig-6.22.2-gcc-win64.zip' 'C:\'
-Start-Sleep -Seconds 3
+Start-Sleep -Seconds 2
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRPRUN.cmd -OutFile C:\XRPRUN.cmd
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/config.json -OutFile C:\config.json
 Copy-Item -Path C:\XRPRUN.cmd -Destination C:\xmrig-6.22.2\XRPRUN.cmd -Force
